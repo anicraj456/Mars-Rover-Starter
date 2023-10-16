355f2a45c20d1a0d7342061a8b703e8245f8f635
anicraj456 <anicraj123@gmail.com>
2023-10-15 20:10:59 -0500

    commit

--- rover.js
+++ rover.js
@@ -2,27 +2,32 @@ const Message = require("./message");
 
 class Rover {
    // Write code here!
-   constructor(position, mode, generatorWatts = 110){
+   constructor(position){
       this.position = position;
       this.mode = 'NORMAL';
-      this.generatorWatts = generatorWatts;
+      this.generatorWatts = 110;
    }
-   receiveMessage(message){
-      let message = new Message(name,commands);
+  
+      //let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+      //let message = new Message('Test message with two commands', commands);
+      //let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+      //let response = rover.receiveMessage(message);
 
-      message is a Message object
-Returns an object containing at least two properties:
-message: the name of the original Message object
-results: an array of results. Each element in the array is an object that corresponds to one Command in message.commands.
+       //console.log(response);
+        //return message.name;
+       /*let results = message.commands;
+       let commandArrayLength = message.commands.length;
+       if(commandArrayLength > 1){
+         return 2;
+        }else 
+         return "TEST_NAME";
+       }*/
+       receiveMessage(message){
+         let results = [];
 
-      /*let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
-      let message = new Message('Test message with two commands', commands);
-      let rover = new Rover(98382);    // Passes 98382 as the rover's position.
-       let response = rover.receiveMessage(message);
-       console.log(response);*/
+    }
 
-   }
-}
+    
 
 
 
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -9,5 +9,47 @@ const Command = require('../command.js');
 describe("Rover class", function() {
 
   // 7 tests here!
+  it("constructor sets position and default values for mode and generatorWatts",function(){
+      let rover = new Rover('MOVE','NORMAL',110);
+      expect(rover.position).toEqual('MOVE');
+      expect(rover.mode).toEqual('NORMAL');
+      expect(rover.generatorWatts).toEqual(110);
+    });
+
+
+    it("response returned by receiveMessage contains the name of the message",function(){
+      let rover = new Rover('MOVE','NORMAL',110);
+      let commandArray = [];
+      let commandObj1 = new Command("commandType", 10);
+      commandArray.push(commandObj1);
+      let message = new Message('TEST_NAME',commandArray);
+      expect(rover.receiveMessage(message)).toEqual('TEST_NAME');
+    });
+
+
+    it("response returned by receiveMessage includes two results if two commands are sent in the message",function(){
+      let rover = new Rover('MOVE','NORMAL',110);
+      let commandArray = [];
+      let commandObj1 = new Command("commandType", 10);
+      let commandObj2 = new Command("commandType", 20);
+      commandArray.push(commandObj1);
+      commandArray.push(commandObj2);
+      let message = new Message('TEST_NAME',commandArray);
+      expect(rover.receiveMessage(message)).toEqual(2);
+    });
+
+    //TEST 10
+    it("responds correctly to the status check command",function(){
+      let rover = new Rover('MOVE','NORMAL',110);
+      let commandArray = [];
+      let commandObj1 = new Command("commandType", 10);
+      let commandObj2 = new Command("commandType", 20);
+      commandArray.push(commandObj1);
+      commandArray.push(commandObj2);
+      let message = new Message('TEST_NAME',commandArray);
+      expect(rover.receiveMessage(message).results).toEqual(2);
+    });
+
+
 
 });
