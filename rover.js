f8d5b44728d648327c0bac2e82fb7c13153a6616
anicraj456 <anicraj123@gmail.com>
2023-10-15 21:22:26 -0500

    working rover

--- rover.js
+++ rover.js
@@ -7,24 +7,12 @@ class Rover {
       this.mode = 'NORMAL';
       this.generatorWatts = 110;
    }
-  
-      //let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
-      //let message = new Message('Test message with two commands', commands);
-      //let rover = new Rover(98382);    // Passes 98382 as the rover's position.
-      //let response = rover.receiveMessage(message);
 
-       //console.log(response);
-        //return message.name;
-       /*let results = message.commands;
-       let commandArrayLength = message.commands.length;
-       if(commandArrayLength > 1){
-         return 2;
-        }else 
-         return "TEST_NAME";
-       }*/
+
   receiveMessage(message){
          const results = [];
-    console.log("commands------"+message.commands.length);
+        //console.log("commands------"+message.commands.length);
+
       for(let i=0;i<message.commands.length;i++){
         const tempObject = message.commands[i];
         const command = tempObject.commandType;
@@ -32,15 +20,11 @@ class Rover {
         const result = this.process(command, value);
         results.push(result); 
     }
-
     return { name: message.name, results: results };
   }
 
-
-    process(command, value){
-
+   roverStatus(command, value){
       let result = {}
-
       if(command == 'STATUS_CHECK'){
         result = {
           completed: true,
@@ -66,19 +50,9 @@ class Rover {
       }else{
         result.completed = false;
         result.message = 'UNKNOWN COMMAND';
-
       }
-
       return result;
-
     }
-
-    //return { name: message.name, results: results };
   }
-
-
-    
-
-
 
 module.exports = Rover;
